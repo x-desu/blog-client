5af0e74e8ebd0b3108d37ffac374ed7d47fd828e
x-desu <amitesh5q2@gmail.com>
2025-01-04 17:38:15 +0530

    loaders added

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -42,7 +42,7 @@ const PostList = () => {
 
   const allPosts = data?.pages?.flatMap(page=>page.posts) || []
   const loading = status === "loading" || isFetching;
-  if(loading){
+  if(loading && !hasNextPage){
     return(
       <SkeletonTheme baseColor={theme==='dark'?"#202020":"#ebebeb"} highlightColor={theme==='dark'?'#444':"#fbfbfb"}>
       <div className="flex xl:flex-row flex-col  gap-4">
@@ -63,7 +63,22 @@ const PostList = () => {
     dataLength={allPosts.length} //This is important field to render the next data
     next={fetchNextPage}
     hasMore={!!hasNextPage}
-    loader={<h4>Loading more posts...</h4>}
+    loader={
+      isFetchingNextPage ? (
+        <SkeletonTheme baseColor={theme==='dark'?"#202020":"#ebebeb"} highlightColor={theme==='dark'?'#444':"#fbfbfb"}>
+        <div className="flex xl:flex-row flex-col  gap-4">
+        {<Skeleton  className=" rounded-2xl md:hidden h-60 sm:h-96 md:h-40 xl-40 2xl:h-60" containerClassName="rounded-2xl xl:block xl:w-1/3 md:hidden" enableAnimation={true}/>||<h1>loading...</h1>}
+        <div className="flex flex-col xl:w-2/3 gap-4">
+        {<Skeleton count={2} className="animate-pulse " containerClassName="md:text-3xl text-3xl " enableAnimation={true}/>||<h1>loading...</h1>}
+        {<Skeleton count={1} className="animate-pulse " containerClassName="w-1/3" enableAnimation={true}/>||<h1>loading...</h1>}
+        {<Skeleton count={3} className="animate-pulse" enableAnimation={true}/>||<h1>loading...</h1>}
+        {<Skeleton count={1} className="animate-pulse" containerClassName="w-1/5" enableAnimation={true}/>||<h1>loading...</h1>}
+        </div>
+        </div>
+        </SkeletonTheme>
+      ) : null
+    }
+    scrollThreshold={0.8}
     endMessage={
       <p>
         <b>All posts loaded</b>
